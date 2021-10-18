3deba394a564a12051f10934013f95c9cf27dbea
Your Name <c5sc3.gri.n4w7@gamil.com>
2021-10-18 13:23:38 +0900

    ✨feat: add blog.md

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,17 @@
+<template>
+  <article>
+    <h1>{{ article.title }}</h1>
+    <NuxtContent :document="article" />
+  </article>
+</template>
+ 
+<script>
+export default {
+  async asyncData ({ $content, params }) {
+    const article = await $content('blog', params.slug).fetch()
+    return {
+      article
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,25 @@
+	
 <template>
-  <Tutorial/>
+  <ul>
+    <li v-for="article in blog" :key="article.slug">
+      <nuxt-link :to="'/blog/' + article.slug">
+        <time :datetime="article.createdAt">
+          {{ $dateFns.format(new Date(article.createdAt), 'yyyy/MM/dd') }}
+        </time>
+        <p>{{ article.title }}</p>
+      </nuxt-link>
+    </li>
+  </ul>
 </template>
-
+ 
 <script>
-export default {}
+export default {
+  async asyncData ({ $content }) {
+    // 記事を全て取得（作成日で降順にソート）
+    const blog = await $content('blog').sortBy('createdAt', 'desc').fetch()
+    return {
+      blog
+    }
+  }
+}
 </script>
